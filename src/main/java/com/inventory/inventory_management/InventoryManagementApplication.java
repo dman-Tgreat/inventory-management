8f7a72978d53e27f84972236ddca7d6368564e31
Dagmawi Mitiku <mitikudagmawi0@gmail.com>
2026-06-22 18:56:39 +0300

    removed un used dependency

--- src/main/java/com/inventory/inventory_management/InventoryManagementApplication.java
+++ src/main/java/com/inventory/inventory_management/InventoryManagementApplication.java
@@ -3,11 +3,12 @@ package com.inventory.inventory_management;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class InventoryManagementApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(InventoryManagementApplication.class, args);
+			SpringApplication.run(InventoryManagementApplication.class, args);	
 	}
 
 }
