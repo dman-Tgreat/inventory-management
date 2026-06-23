97d56380d4669dce29bf76ccac94ef25b5b451c9
Dagmawi Mitiku <mitikudagmawi0@gmail.com>
2026-06-23 14:27:58 +0300

    moved the spring into a separate directory

--- src/main/java/com/inventory/inventory_management/service/Interface/DashboardService.java
+++
@@ -1,7 +0,0 @@
-package com.inventory.inventory_management.service.Interface;
-
-import com.inventory.inventory_management.dto.DashboardResponse;
-
-public interface DashboardService {
-    DashboardResponse getDashboard();
-}
--- src/main/java/com/inventory/inventory_management/util/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.inventory.inventory_management.util;
-
-public final class Constants {
-
-    public static final String API_PREFIX = "/api";
-
-    private Constants() {
-    }
-}
